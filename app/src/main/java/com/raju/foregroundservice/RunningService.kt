1bd1d3ee68439ddef88acdb9227fba05900a9f67
Raju Chettri <dada@Rajus-MBP.ht.home>
2024-02-13 11:24:45 -0500

    forground service implemented

---
+++ app/src/main/java/com/raju/foregroundservice/RunningService.kt
@@ -0,0 +1,35 @@
+package com.raju.foregroundservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+
+class RunningService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            Actions.START.toString() -> startService()
+            Actions.STOP.toString() -> stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startService() {
+        val notification = NotificationCompat.Builder(this, "running_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Run is active")
+            .setContentText("Elapsed time: 00:50")
+            .build()
+        startForeground(1, notification)
+    }
+
+    enum class Actions {
+        START, STOP
+    }
+}
